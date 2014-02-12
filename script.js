8575c5c3d3e2bc9fb6e2ff2e4caf4ffdd6f60201
Tylor Sherman <tylor@denimandsteel.com>
2014-02-12 11:34:49 -0800

    Use access token instead of our little heroku server

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 $(function() {
-  $.get('http://gene-cafe.herokuapp.com/', function(payload) {
-    payload.forEach(function(item, index) {
-      // var img = $('img').attr('src', );
-      // console.log(img)
-      $('body').append('<img src="' + item.images.low_resolution.url + '">');
+  // $.get('http://gene-cafe.herokuapp.com/', function(payload) {
+  //   payload.forEach(function(item, index) {
+  //     // var img = $('img').attr('src', );
+  //     // console.log(img)
+  //     $('body').append('<img src="' + item.images.low_resolution.url + '">');
+  //   });
+  // });
+  $.ajax({
+    dataType: 'jsonp',
+    url: 'https://api.instagram.com/v1/locations/229295/media/recent',
+    data: {
+      access_token: '1090248051.1d1d36f.d24a1a83cfc14bb8b337245827769d42'
+    }
+  }).done(function(payload) {
+    payload.data.forEach(function(item, index) {
+      // var resolution = 'thumbnail';
+      var resolution = 'low_resolution';
+      // var resolution = 'standard_resolution';
+      $image = $('<img>').attr('src', item.images[resolution].url).attr('width', item.images[resolution].width).attr('height', item.images[resolution].height);
+      $('body').append($image);
     });
   });
 });
