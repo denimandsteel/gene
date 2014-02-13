13cd26cd4119ea19994cb29610fbd09610a52263
Tylor Sherman <tylor@denimandsteel.com>
2014-02-12 16:13:38 -0800

    Clean up a bit

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 $(function() {
-  FastClick.attach(document.body);
   var currentURL = 'https://api.instagram.com/v1/locations/229295/media/recent';
 
-  function loadMore()
-  {
-    console.log('load more called!');
+  function loadImages() {
     $.ajax({
       dataType: 'jsonp',
       url: currentURL,
@@ -12,7 +9,6 @@ $(function() {
         access_token: '1090248051.1d1d36f.d24a1a83cfc14bb8b337245827769d42'
       }
     }).done(function(payload) {
-      console.log(payload);
       if (currentURL != payload.pagination.next_url) {
         currentURL = payload.pagination.next_url
         payload.data.forEach(function(item, index) {
@@ -20,45 +16,39 @@ $(function() {
           $image = $('<img>').attr('src', item.images['standard_resolution'].url).attr('width', item.images[resolution].width).attr('height', item.images[resolution].height);
           $('body').append($image);
         });
-
-        $(window).bind('scroll', bindScroll);
+        $(window).bind('scroll', infiniteScroll);
       }
-
     });
   }
 
-  function bindScroll(){
-    if($(window).scrollTop() + $(window).height() > $(document).height() - 800) {
-       $(window).unbind('scroll', bindScroll);
-       loadMore();
+  function infiniteScroll() {
+    if ($(window).scrollTop() + $(window).height() > $(document).height() - 800) {
+      $(window).unbind('scroll', infiniteScroll);
+      loadImages();
     }
   }
 
-  loadMore();
-
-  $(window).bind('resize', function() {
-    bindScrollToMovement();
-  });
-
-  function bindScrollToMovement() {
+  function resize() {
     if ($(window).width() > 767) {
-      $(window).bind('scroll', moveThings);
-      moveThings();
-    } else {
-      $(window).unbind('scroll', moveThings);
-      $('#hours, #location, #contact').css('-webkit-transform', 'translate(0, 0)');
+      $(window).bind('scroll', moveCards);
+      moveCards();
+    }
+    else {
+      $(window).unbind('scroll', moveCards);
+      $('#hours').css('-webkit-transform', 'rotate(-2deg) translate(0, 0)');
+      $('#location, #contact').css('-webkit-transform', 'translate(0, 0)');
     }
   }
 
-  function moveThings() {
+  function moveCards() {
     var top = $(window).scrollTop();
-    $('#hours').css('-webkit-transform', 'translate(-90%,' + (-70 - top/3.5) + '%)');
+    $('#hours').css('-webkit-transform', 'rotate(-2deg) translate(-90%,' + (-70 - top/3.5) + '%)');
     $('#location').css('-webkit-transform', 'translate(20%,' + (-60 - top/2.5) + '%)');
     $('#contact').css('-webkit-transform', 'translate(-50%,' + (20 - top/2) + '%)');
   }
 
-  bindScrollToMovement();
+  $(window).bind('resize', resize);
+  loadImages();
+  resize();
   
 });
-
-
